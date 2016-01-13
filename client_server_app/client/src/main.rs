7580e80d10ee498f67990ebe1c1d19c38eb22238
AlexNav73 <fifthhorseman@mail.ru>
2016-01-13 22:09:22 +0300

    Update repo

--- client_server_app/client/src/main.rs
+++ client_server_app/client/src/main.rs
@@ -1,6 +1,12 @@
 
 use std::net::*;
 use std::io::BufRead;
+use std::time::Duration;
+use std::sync::{Arc, Mutex};
+
+const STATUS_ACK: u8 = 200;
+const STATUS_SYN: u8 = 100;
+const TIMEOUT: usize = 20;
 
 struct Client {
     socket: UdpSocket,
@@ -8,35 +14,80 @@ struct Client {
 
 impl Client {
     fn new(addr: &str) -> Client {
-        Client {
-            socket: UdpSocket::bind(addr).unwrap()
-        }
+        Client { socket: UdpSocket::bind(addr).unwrap() }
     }
 
     fn get_message(&mut self) -> String {
         let stdin = std::io::stdin();
-        let mut input = stdin.lock();
-        let mut user_in = String::new();
-        input.read_line(&mut user_in).unwrap();
-        user_in.pop();
-        user_in
+        let mut message = String::new();
+        stdin.read_line(&mut message).unwrap();
+
+        let mut ret_val = String::new();
+        ret_val.push_str(message.trim());
+        ret_val
     }
 
     fn start(&mut self, dest: &str) {
         loop {
             let message = self.get_message();
             let mess_len = message.len();
-            match self.socket.send_to(&message.into_bytes(), dest) {
+            match self.socket.send_to(message.as_bytes(), dest) {
                 Ok(n) => println!("Log: {} bytes has been sended", n),
-                Err(e) => panic!("Log: send error: {}", e)
+                Err(e) => panic!("Log: send error: {}", e),
             };
-            if mess_len == 0 { break; }
+            if mess_len == 0 {
+                break;
+            }
+        }
+    }
+
+    fn send_request_to_connect(mutex: Arc<Mutex<UdpSocket>>, dest: &str, duration: &Duration) {
+        for _ in 0..TIMEOUT {
+            let socket = mutex.lock().unwrap();
+            socket.send_to(&[STATUS_SYN; 1], dest);
+            std::thread::sleep(*duration);
+        }
+    }
+
+    #[allow(unused)]
+    fn try_to_connect(&mut self, dest: &str) -> bool {
+        let duration = Duration::from_millis(100);
+        let mut buff: [u8; 1] = [0u8];
+        let socket = Arc::new(Muxtex::new(self.socket));
+
+        let s = socket.clone();
+        std::thread::spawn(|| { send_request_to_connect(s, dest, &duration) });
+
+        let socket = mutex.lock().unwrap();
+        match socket.recv_from(&mut buff) {
+            Ok((readed, from)) if readed == 1 => {
+                if buff[0] == STATUS_ACK {
+                    return true;
+                } else if buff[0] == STATUS_SYN {
+                    socket.send_to(&[STATUS_ACK; 1], from);
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+            Err(e) => {
+                println!("Log: error occured {}", e);
+                return false;
+            }
+            _ => unreachable!()
         }
     }
 }
 
 fn main() {
-    let mut client = Client::new("127.0.0.1:6655");
-    client.start("127.0.0.1:6666");
+    let mut ips = std::env::args();
+    let from = &ips.nth(1).unwrap_or("127.0.0.1:5555".to_string());
+    let to = &ips.next().unwrap_or("127.0.0.1:6655".to_string());
+    //let to = &ips.next().unwrap_or("192.164.1.2:6655".to_string());
+
+    let mut client = Client::new(from);
+    if client.try_to_connect(to) {
+        client.start(to);
+    }
     drop(client.socket);
 }
--- client_server_app/server/src/main.rs
+++ client_server_app/server/src/main.rs
@@ -1,35 +1,56 @@
 
 use std::net::*;
 
+const BUFF_SIZE: usize = 100;
+
 struct Server {
     socket: UdpSocket,
-    buf: [u8; 100]
+    buf: [u8; BUFF_SIZE],
+    user_connected: bool
 }
 
 impl Server {
     fn new(addr: &str) -> Server {
         Server {
             socket: UdpSocket::bind(addr).unwrap(),
-            buf: [0; 100]
+            buf: [0; BUFF_SIZE],
+            user_connected: false
         }
     }
 
+    #[allow(unused)]
     fn start(&mut self) {
         loop {
-            let (recved, _) = self.socket.recv_from(&mut self.buf).unwrap();
-            println!("Log: recive {} bytes", recved);
+            let (recved, from) = self.socket.recv_from(&mut self.buf).unwrap();
+            println!("Log: recive {} bytes from {}", recved, from);
+
+            if recved == 1 && self.buf[0] == 100u8 && !self.user_connected {
+                println!("Log: user tries to connect");
+                self.socket.send_to(&[200u8; 1], from);
+                self.user_connected = true;
+            }
 
             let mut vec = Vec::with_capacity(self.buf.len());
-            for byte in self.buf[..recved].iter() { vec.push(*byte); }
+            for byte in self.buf[..recved].iter() {
+                vec.push(*byte);
+            }
+
             let message = String::from_utf8(vec).unwrap();
-            if message.len() == 0 { break; }
+            if message.len() == 0 {
+                break;
+            }
+
             println!("{}", message);
         }
     }
 }
 
 fn main() {
-    let mut server = Server::new("127.0.0.1:6666");
+    let mut ip = std::env::args();
+    let to = &ip.nth(1).unwrap_or("127.0.0.1:6655".to_string());
+
+    let mut server = Server::new(to);
     server.start();
+
     drop(server.socket);
 }
