bdfc3c0eab80630e01d28bd17c3830c11edbe0c9
AlexNav73 <fifthhorseman@mail.ru>
2016-01-28 16:02:20 +0300

    Implement enumeration over processes (need to fix process names printing)

--- enum_processes/src/main.rs
+++ enum_processes/src/main.rs
@@ -3,105 +3,126 @@ extern crate winapi;
 extern crate psapi;
 extern crate kernel32;
 
-use winapi::minwindef::{DWORD, HMODULE, LPDWORD, BOOL};
-use winapi::winnt::{HANDLE, LPWSTR, WCHAR, PROCESS_QUERY_INFORMATION, PROCESS_VM_READ};
-use kernel32::{K32EnumProcesses, K32EnumProcessModules, CloseHandle, K32GetModuleBaseNameW, OpenProcess};
+use kernel32::{CreateToolhelp32Snapshot, Process32First, Process32Next, CloseHandle};
+use winapi::minwindef::{DWORD, MAX_PATH};
+use winapi::tlhelp32::{TH32CS_SNAPPROCESS, PROCESSENTRY32, LPPROCESSENTRY32};
+use winapi::shlobj::{INVALID_HANDLE_VALUE};
+use winapi::winnt::{HANDLE};
 
-struct Process {
-    a_procs: [DWORD; 1024],
-    len: usize
+struct ProcessesSnapshot {
+    inner: Option<HANDLE>
 }
 
-impl Process {
+impl ProcessesSnapshot {
 
-    pub fn new() -> Result<Process, ()> {
+    pub fn new() -> Result<ProcessesSnapshot, ()> {
+        let h_processes_snap = unsafe { CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0) };
+        if h_processes_snap == INVALID_HANDLE_VALUE {
+            return Err(());
+        }
 
-        let mut pe = Process {
-            a_procs: unsafe { std::mem::uninitialized() },
-            len: 0
-        };
+        Ok(ProcessesSnapshot {
+            inner: Some(h_processes_snap)
+        })
+    }
 
-        let mut cb_needed: DWORD = 0;
+    //fn get_process_name(&self, procId: DWORD) -> Result<String, ()> {
 
-        let a_procs_ref: [usize; 2] = unsafe { std::mem::transmute(pe.a_procs.as_mut()) };
-        let a_procs_ptr = a_procs_ref[0] as *mut DWORD;
-        let cb_needed_ptr = &mut cb_needed as *mut u32;
+        //let mut ret_val = Err(());
+        //let h_proc: HANDLE = unsafe { OpenProcess(PROCESS_QUERY_INFORMATION | PROCESS_VM_READ, 0 as BOOL, procId) };
+
+        //if !h_proc.is_null() {
+
+            //let mut h_mod: HMODULE = unsafe { std::mem::uninitialized() };
+            //let mut cb_needed: DWORD = 0;
+            //let mut name: [WCHAR; 1024] = unsafe { std::mem::uninitialized() };
+
+            //unsafe {
+                //if K32EnumProcessModules(h_proc, &mut h_mod as *mut HMODULE, std::mem::size_of::<HMODULE>() as DWORD, &mut cb_needed as LPDWORD) != -1 {
+                    //let mut name_ref: [usize; 2] = unsafe { std::mem::transmute(name.as_mut()) };
+                    //let name_ptr = name_ref[0] as LPWSTR;
+                    //K32GetModuleBaseNameW(h_proc, h_mod, name_ptr, 1024);
+
+                    ////unsafe { ret_val = Ok(std::ffi::CString::from_raw(std::mem::transmute(&mut name)).into_string().unwrap()) }
+                    //ret_val = Ok("Name".to_string());
+                //}
+
+                //CloseHandle(h_proc);
+            //}
+        //}
+        //ret_val
+    //}
+}
+
+fn new_process_entry() -> PROCESSENTRY32 {
+    PROCESSENTRY32 {
+        dwSize: std::mem::size_of::<PROCESSENTRY32>() as DWORD,
+        cntUsage: 0,
+        th32ProcessID: 0,
+        th32DefaultHeapID: 0,
+        th32ModuleID: 0,
+        cntThreads: 0,
+        th32ParentProcessID: 0,
+        pcPriClassBase: 0,
+        dwFlags: 0,
+        szExeFile: [' ' as i8; MAX_PATH],
+    }
+}
+
+struct ProcessIter {
+    snapshot: ProcessesSnapshot,
+    pe32: PROCESSENTRY32
+}
+
+impl IntoIterator for ProcessesSnapshot {
+    type Item = String;
+    type IntoIter = ProcessIter;
+
+    fn into_iter(self) -> ProcessIter {
+
+        let mut pe32 = new_process_entry();
 
         unsafe {
-            if K32EnumProcesses(a_procs_ptr, pe.a_procs.len() as u32, cb_needed_ptr) != -1 {
-                pe.len = (cb_needed / std::mem::size_of::<DWORD>() as DWORD) as usize;
-                Ok(pe)
-            } else {
-                Err(())
-            }
-        }
-
-    }
-
-    fn get_process_name(&self, procId: DWORD) -> Result<String, ()> {
-
-        let mut ret_val = Err(());
-        let h_proc: HANDLE = unsafe { OpenProcess(PROCESS_QUERY_INFORMATION | PROCESS_VM_READ, 0 as BOOL, procId) };
-
-        if !h_proc.is_null() {
-
-            let mut h_mod: HMODULE = unsafe { std::mem::uninitialized() };
-            let mut cb_needed: DWORD = 0;
-            let mut name: [WCHAR; 1024] = unsafe { std::mem::uninitialized() };
-
-            unsafe {
-                if K32EnumProcessModules(h_proc, &mut h_mod as *mut HMODULE, std::mem::size_of::<HMODULE>() as DWORD, &mut cb_needed as LPDWORD) != -1 {
-                    let mut name_ref: [usize; 2] = unsafe { std::mem::transmute(name.as_mut()) };
-                    let name_ptr = name_ref[0] as LPWSTR;
-                    K32GetModuleBaseNameW(h_proc, h_mod, name_ptr, 1024);
-
-                    //unsafe { ret_val = Ok(std::ffi::CString::from_raw(std::mem::transmute(&mut name)).into_string().unwrap()) }
-                    ret_val = Ok("Name".to_string());
+            if let Some(s) = self.inner {
+                if Process32First(s, &mut pe32 as LPPROCESSENTRY32) == 0 {
+                    CloseHandle(s);
                 }
-
-                CloseHandle(h_proc);
             }
-        } 
-        ret_val
-    }
-}
-
-struct ProcessIter<'a> {
-    inner: &'a mut Process,
-    ind: usize
-}
-
-impl<'a> IntoIterator for &'a mut Process {
-    type Item = String;
-    type IntoIter = ProcessIter<'a>;
-
-    fn into_iter(self) -> ProcessIter<'a> {
-        ProcessIter {
-            inner: self,
-            ind: 0
         }
+
+        ProcessIter {
+            snapshot: self,
+            pe32: pe32
+        }
+
     }
 }
 
-impl<'a> Iterator for ProcessIter<'a> {
+impl Iterator for ProcessIter {
     type Item = String;
 
     fn next(&mut self) -> Option<Self::Item> {
-        if self.ind < self.inner.len {
-            self.ind += 1;
-            let process_id = self.inner.a_procs[self.ind];
-            let process_name = self.inner.get_process_name(process_id).unwrap();
-            Some(format!("Name: {} PID: {}", process_name, process_id))
-        } else {
-            None
-        }
+
+        use std::ffi::*;
+        let string = unsafe { CString::from_raw(self.pe32.szExeFile[..].as_mut_ptr()) };
+        let ret = String::from_utf8_lossy(string.as_bytes());
+
+        if let Some(snap) = self.snapshot.inner {
+            unsafe {
+                if Process32Next(snap, &mut self.pe32 as LPPROCESSENTRY32) != 0 {
+                    Some(ret.into_owned())
+                } else {
+                    None
+                }
+            }
+        } else { None }
     }
 }
 
 fn main() {
 
-    let mut pe = match Process::new() {
-        Ok(ref mut pe) => {
+    match ProcessesSnapshot::new() {
+        Ok(pe) => {
             for p in pe.into_iter() {
                 println!("{}", p);
             }
