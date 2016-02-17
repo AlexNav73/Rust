a65bc0bdfe3ac165641924b0c8f766a1d9afe92a
Aliaksandr_Navitski <Aliaksandr_Navitski@epam.com>
2016-02-17 16:19:42 +0300

    Update ...

---
+++ MangaDb/MangaDb/Helpers/CsvHelper.cs
@@ -0,0 +1,30 @@
+﻿using CsvHelper;
+using MangaDb.Entities;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MangaDb.Helpers
+{
+    public class CsvHelper
+    {
+        public List<T> GetRecords<T>(string fileName)
+        {
+            using (var reader = new CsvReader(new StreamReader(File.Open(fileName, FileMode.OpenOrCreate))))
+            {
+                return reader.GetRecords<T>().ToList();
+            }
+        }
+
+        public void SaveRecords<T>(string fileName, List<T> records)
+        {
+            using (var writer = new CsvWriter(new StreamWriter(File.Open(fileName, FileMode.OpenOrCreate))))
+            {
+                writer.WriteRecords(records);
+            }
+        }
+    }
+}
---
+++ MangaDb/MangaDb/Modules/CsvStorage.cs
@@ -0,0 +1,17 @@
+﻿using MangaDb.Modules;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MangaDb.Modules
+{
+    public class CsvStorage : IModule
+    {
+        public Task<object> Execute(object context)
+        {
+            return null;
+        }
+    }
+}
